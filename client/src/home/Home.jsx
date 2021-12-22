03667c75c3d61f94ed1ddbd99284fcd3fdb0cdf9
jonandonigv <jonandonigv@gmail.com>
2021-12-22 17:47:33 +0100

    Featured Movie/Series

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div className='home'>
             <Navbar />
+            <Featured />
         </div>
     )
 }
