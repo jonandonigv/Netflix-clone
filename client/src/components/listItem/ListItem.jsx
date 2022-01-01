14cf502a1349ba2abb3ec3b619d8d8ae3497c335
jonandonigv <jonandonigv@gmail.com>
2022-01-01 17:44:14 +0100

    change need to test

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -5,7 +5,7 @@ import AddIcon from '@mui/icons-material/Add';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
 
-export default function ListItem(index) {
+export default function ListItem(props) {
     const [isHovered, setIsHovered] = useState(false);
     const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
     return (
@@ -13,7 +13,7 @@ export default function ListItem(index) {
             className="listItem"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-            style={{left: isHovered && (index * 225 - 50 + index * 2.5) }}>
+            style={{left: isHovered && (props.index * 225 - 50 + props.index * 2.5) }}>
             <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" 
             alt="" />
             <div className="itemInfo">
