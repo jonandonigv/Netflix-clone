ffd760504d9d0b8471def13fde2892ab424e3de9
jonandonigv <jonandonigv@gmail.com>
2022-01-03 10:07:17 +0100

    Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import './app.scss'
 import Home from "./pages/home/Home";
+import Login from './pages/Login/Login';
+import Register from './pages/register/Register';
+import Watch from './pages/watch/Watch';
 
 function App() {
   return (
-    <Home /> 
+    <Login /> 
   );
 }
 
