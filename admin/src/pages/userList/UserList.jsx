cc8b7b23c00322eb745a71682e518c327a683edb
jonandonigv <jonandonigv@gmail.com>
2022-01-04 21:48:50 +0100

    UserList page created

---
+++ admin/src/pages/userList/UserList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './userList.scss'
+
+export default function UserList() {
+    return (
+        <div className='userList'>
+            
+        </div>
+    )
+}
