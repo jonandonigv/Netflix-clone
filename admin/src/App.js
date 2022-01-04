0b465883104a7b60f72a5ad20849fefe1abdb3d2
jonandonigv <jonandonigv@gmail.com>
2022-01-04 18:41:11 +0100

    Basic routing

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,15 +1,26 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import './app.scss';
+import Home from "./pages/Home/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Topbar />
       <div className="container">
         <Sidebar />
+        <Routes >
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/users' element={<UserList/>}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
