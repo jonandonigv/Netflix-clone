94334a37d6476bb0dfa363c813c78a2c84df9af3
jonandonigv <jonandonigv@gmail.com>
2022-01-04 15:56:26 +0100

    style added

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
