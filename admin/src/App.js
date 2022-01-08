6fe3fb9930652f3fc98a6eb236203c9fe0b10f03
jonandonigv <jonandonigv@gmail.com>
2022-01-08 15:42:26 +0100

    stuff

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,6 +8,9 @@ import {
   Route,
 } from "react-router-dom"
 import UserList from "./pages/userList/UserList";
+import { User } from "./pages/user/User";
+import { NewUser } from "./pages/newUser/NewUser";
+import { MovieList} from "./pages/movieList/MovieList";
 
 function App() {
   return (
@@ -18,6 +21,11 @@ function App() {
         <Routes >
           <Route exact path='/' element={<Home/>}/>
           <Route exact path='/users' element={<UserList/>}/>
+          <Route exact path='/user/:userId' element={<User />}/>
+          <Route exact path='/newUser' element={<NewUser />}/>
+          <Route exact path='/movies' element={<MovieList />}/>
+          <Route exact path='/movie/:movieId' element={<User />}/>
+          <Route exact path='/newMovie' element={<NewUser />}/>
         </Routes>
       </div>
     </Router>
