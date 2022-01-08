b32368d49d3420d288d25b8d016891d1ea797aee
jonandonigv <jonandonigv@gmail.com>
2022-01-08 17:22:51 +0100

    New Movie

---
+++ admin/src/pages/newMovie/NewMovie.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './newMovie.scss'
+
+export const NewMovie = () => {
+    return (
+        <div className='newMovie'>
+            <h1 className="newUser">New Movie</h1>
+            <form action="" className="newUserForm">
+                <div className="newUserItem">
+                    <label>Title</label>
+                    <input type="text" placeholder='title' />
+                </div>
+                <div className="newUserItem">
+                    <label>Full Name</label>
+                    <input type="text" placeholder='Full name' />
+                </div>
+                <div className="newUserItem">
+                    <label>Email</label>
+                    <input type="text" placeholder='email@email.com' />
+                </div>
+                <div className="newUserItem">
+                    <label>Password</label>
+                    <input type="password" placeholder='password' />
+                </div>
+                <div className="newUserItem">
+                    <label>Phone</label>
+                    <input type="text" placeholder='phone' />
+                </div>
+                <div className="newUserItem">
+                    <label>Address</label>
+                    <input type="text" placeholder='address' />
+                </div>
+                <div className="newUserItem">
+                    <label>Gender</label>
+                    <div className="newUserGender">
+                        <input type="radio" name='gender' id='male' value='male' />
+                        <label htmlFor="male">Male</label>
+                        <input type="radio" name='gender' id='female' value='female' />
+                        <label htmlFor="female">Female</label>
+                        <input type="radio" name='gender' id='other' value='other' />
+                        <label htmlFor="other">Other</label>
+                    </div>
+                </div>
+                <div className="newUserItem">
+                    <label>Active</label>
+                    <select name="active" id="active" className="newUserSelect">
+                        <option value='yes'>Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+                <button className="newUserButton">
+                    Create
+                </button>
+            </form>
+        </div>
+    )
+}
