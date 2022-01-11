f12089a2cba06452c394cc1bef0143f7a96ed395
jonandonigv <jonandonigv@gmail.com>
2022-01-11 12:49:50 +0100

    Context and login

--- admin/src/context/authContext/AuthReducer.js
+++ admin/src/context/authContext/AuthReducer.js
@@ -18,6 +18,25 @@ const AuthReducer = (state, action) => {
                 isFeching: false,
                 error: true
             };
+        
+            case "LOGOUT_START":
+                return {
+                    user: null,
+                    isFeching: false,
+                    error: false
+                };
+            case "LOGOUT_SUCCESS":
+                return {
+                    user: action.payload,
+                    isFeching: false,
+                    error: true
+                };
+            case "LOGOUT_FAILURE":
+                return {
+                    user: null,
+                    isFeching: false,
+                    error: true
+                };
         default:
             return { ...state};
     }
--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
+import { useContext } from 'react';
 import './login.scss'
+import { login } from '../../context/authContext/ApiCall';
+import { AuthContext } from '../../context/authContext/AuthContext';
 
 export const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const {isFetching, dispathc} = useContext(AuthContext);
+
     const handleLogin = (e) => {
         e.preventDefault();
-        
+        login({email, password}, dispathc);
     }
     return (
         <div className='login'>
             <form action="" className="loginForm">
                 <input type="text" placeholder='email' className="loginInput" onChange={(e) => {setEmail(e.value)}}/>
                 <input type="password" placeholder='password' className="loginInput" onChange={(p) => setPassword(p.value)}/>
-                <button className='loginButton' onClick={handleLogin()}>Login</button>
+                <button className='loginButton' onClick={handleLogin()} disabled={isFetching}>Login</button>
             </form>
         </div>
     )
