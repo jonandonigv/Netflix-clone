67461f83efe368af73700eca8306135bba6b3e56
jonandonigv <jonandonigv@gmail.com>
2022-01-14 00:04:28 +0100

    Home page

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -26,22 +26,33 @@ export default function Home() {
         
       ], []);
     
-      const [userStats, setUserStats] = useState([]);
+    const [userStats, setUserStats] = useState([]);
     
-      useEffect(() => {
-        const getStats = async () => {
-          try {
-            const res = await axios.get('/users/stats', {headers: {token: "Bearer "}}); // Here goes the token
-            const statsList = res.data.sort((a, b) => {
-                return a._id - b._id;
-            })
-            statsList.map((item) => setUserStats(prev=>[...prev, {name: MONTHS[item._id-1], "New User": item.total}]))
-          } catch (error) {
-            console.log(error);
-          }
+    useEffect(() => {
+
+      const getStats = async () => {
+        try {
+          const res = await axios.get("users/stats", {
+            headers: {
+              token: 
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZGQ5NTMzNjkzZWM3MDU2ZWFlY2Q2YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MjExNDU4OSwiZXhwIjoxNjQyNTQ2NTg5fQ.PWuZrYFxk8WDAcTDW8_bOI2Nf1UrN2vFRoYkbpSlxco"
+            },
+          });
+          const statsList = res.data.sort((a, b) => {
+            return a._id - b._id;
+          });
+          statsList.map((item) => 
+            setUserStats((prev) => [
+              ...prev,
+              { name: MONTHS[item._id - 1], "New User": item.total },
+            ])
+          );
+        } catch (error) {
+          console.log(error);
         }
-        getStats();
-      }, [MONTHS]);
+      };
+      getStats();
+    }, [MONTHS]);
 
     return (
         <div className='home'>
