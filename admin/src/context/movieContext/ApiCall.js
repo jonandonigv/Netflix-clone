3bbfa3973021d0b6c154bbf120220093be6358a5
jonandonigv <jonandonigv@gmail.com>
2022-01-15 21:03:35 +0100

    Get Movies and delete movies

--- admin/src/context/movieContext/ApiCall.js
+++ admin/src/context/movieContext/ApiCall.js
@@ -3,19 +3,36 @@ import {
   getMoviesStart,
   getMoviesSuccess,
   getMoviesFailure,
+  deleteMovieStart,
+  deleteMovieSuccess,
+  deleteMovieFailure,
 } from "./MovieActions";
 
 export const getMovies = async (dispatch) => {
-    dispatch(getMoviesStart());
-    try {
-        const res = await axios.get("http://localhost:8000/api/movies", {headers: {token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken}});
-        console.log(localStorage.getItem("user"))
-        dispatch(getMoviesSuccess(res.data));
-    } catch (error) {
-        dispatch(getMoviesFailure());
-    }
+  dispatch(getMoviesStart());
+  try {
+    const res = await axios.get("http://localhost:8000/api/movies", {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    console.log(localStorage.getItem("user"));
+    dispatch(getMoviesSuccess(res.data));
+  } catch (error) {
+    dispatch(getMoviesFailure());
+  }
 };
 
-export const deleteMovie = () => {}
-
-
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+  try {
+    const res = await axios.delete("http://localhost:8000/api/movies/" + id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMovieSuccess(id));
+  } catch (error) {
+    dispatch(deleteMovieFailure());
+  }
+};
--- admin/src/pages/movieList/MovieList.jsx
+++ admin/src/pages/movieList/MovieList.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './movieList.scss'
 import { DataGrid } from '@mui/x-data-grid';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import { movieRows } from '../../dummyData';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import {MovieContext} from '../../context/movieContext/MovieContext';
+import {getMovies, deleteMovie} from '../../context/movieContext/ApiCall'
+import { useEffect } from 'react';
 
 export const MovieList = () => {
-    const [data, setData] = useState(movieRows);
+    const {movies, dispatch} = useContext(MovieContext);
+
+    useEffect(() => {
+        getMovies(dispatch);
+    }, [dispatch]);
 
     const handleDelete = (id) => {
-        setData(data.filter((item) => item.id !== id));
+        deleteMovie(id, dispatch);
+        // setData(data.filter((item) => item.id !== id));
     };
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
+        { field: '_id', headerName: 'ID', width: 90 },
         { field: 'movie', headerName: 'Movie', width: 200, renderCell: (params) =>{
             return (
                 <div className='movieListMovie'>
                     <img className='movieListImg' src={params.row.img}></img>
-                    {params.row.name}
+                    {params.row.title}
                 </div>
             );
         } },
-        { field: 'stock', headerName: 'stock', width: 200 },
-        {
-          field: 'status',
-          headerName: 'Status',
-          width: 120,
-        },
-        {
-            field: 'transactions',
-            headerName: 'Transaction Volume',
-            width: 160,
-        },
+        { field: 'gender', headerName: 'Genre', width: 100 },
+        { field: 'year', headerName: 'Year', width: 120 },
+        { field: 'limit', headerName: 'Limit', width: 120 },
+        { field: 'isSerries', headerName: 'isSeries', width: 120 },
         {
             field: "action",
             headerName: "Action",
@@ -41,10 +40,10 @@ export const MovieList = () => {
             renderCell: (params) => {
                 return(
                     <>
-                    <Link to={'/movie/' + params.row.id}>
+                    <Link to={{pathname:'/movie/' + params.row._id, movie: params.row}}>
                         <button className="movieListEdit">Edit</button>
                     </Link>
-                    <DeleteOutlineIcon  className='movieListDelete' onClick={() => handleDelete(params.row.id)}/>
+                    <DeleteOutlineIcon  className='movieListDelete' onClick={() => handleDelete(params.row._id)}/>
                     </>
                 );
             }
@@ -55,12 +54,13 @@ export const MovieList = () => {
     return (
         <div className='movieList'>
             <DataGrid
-                rows={data}
+                rows={movies}
                 disableSelectionOnClick
                 columns={columns}
                 pageSize={8}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
+                getRowId={r=>r._id}
             />
         </div>
     )
