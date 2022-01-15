06e585f2c2a937147626fbee72875027d584ed5c
jonandonigv <jonandonigv@gmail.com>
2022-01-15 18:00:34 +0100

    fixed route if user exist

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,46 +1,44 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import './app.scss';
+import "./app.scss";
 import Home from "./pages/Home/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import { User } from "./pages/user/User";
 import { NewUser } from "./pages/newUser/NewUser";
-import { MovieList} from "./pages/movieList/MovieList";
+import { MovieList } from "./pages/movieList/MovieList";
 import { Movie } from "./pages/movie/Movie";
 import { NewMovie } from "./pages/newMovie/NewMovie";
-import { useEffect, useMemo, useState, useContext } from "react";
-import axios from 'axios'
-import { Login } from "./pages/login/Login";
-import {AuthContext} from './context/authContext/AuthContext'
+import { useContext } from "react";
+import Login from "./pages/login/Login";
+import { AuthContext } from "./context/authContext/AuthContext";
 
 function App() {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <Router className="App">
       <Routes>
-        <Route exact path='/login' element={user ? <Home/> : <Login/>}/>
-        {user && (
-          <>
-            <Topbar />
-            <div className="container">
-              <Sidebar />
-              <Route exact path='/users' element={<UserList/>}/>
-              <Route exact path='/user/:userId' element={<User />}/>
-              <Route exact path='/newUser' element={<NewUser />}/>
-              <Route exact path='/movies' element={<MovieList />}/>
-              <Route exact path='/movie/:movieId' element={<Movie />}/>
-              <Route exact path='/newMovie' element={<NewMovie />}/>
-              {/* TODO: List routes */}
-            </div>
-          </>
-        )}
+        <Route path="/login" element={user ? <Navigate to={'/'} /> : <Login />} /> 
       </Routes>
+      {user && (
+        <>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/movies" element={<MovieList />} />
+              <Route path="/movie/:movieId" element={<Movie />} />
+              <Route path="/newMovie" element={<NewMovie />} />
+              {/* TODO: List routes */}
+            </Routes>
+          </div>
+        </>
+      )}
     </Router>
   );
 }
