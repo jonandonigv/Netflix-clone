644f2534cd2e6b091b2f03f9a96bd28f8a8d3030
jonandonigv <jonandonigv@gmail.com>
2022-01-26 17:55:03 +0100

    render all the movie options

--- admin/src/pages/newList/NewList.jsx
+++ admin/src/pages/newList/NewList.jsx
@@ -1,189 +1,97 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import storage from "../../firebase";
 import "./newList.scss";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { createMovie } from "../../context/movieContext/ApiCall";
+import { getMovies, createMovie } from "../../context/movieContext/ApiCall";
+import { createList } from "../../context/listContext/ApiCall";
 import { MovieContext } from "../../context/movieContext/MovieContext";
+import { ListContext } from "../../context/listContext/ListContext";
+import { useNavigate } from "react-router-dom";
 
 export const NewList = () => {
-  const [movie, setMovie] = useState(null);
-  const [img, setImg] = useState(null);
-  const [imgTitle, setImgTitle] = useState(null);
-  const [imgSm, setImgSm] = useState(null);
-  const [trailer, setTrailer] = useState(null);
-  const [video, setVideo] = useState(null);
-  const [uploaded, setUploaded] = useState(0);
-  const { dispatch } = useContext(MovieContext);
+  const [list, setList] = useState(null);
+  const navigation = useNavigate();
+
+  const { dispatch } = useContext(ListContext);
+  const { movies, dispatch: dispatchMovie } = useContext(MovieContext);
+
+  useEffect(() => {
+    getMovies(dispatchMovie);
+  }, [dispatchMovie]);
 
   const handleChange = (e) => {
     const value = e.target.value;
-    setMovie({ ...movie, [e.target.name]: value });
+    setList({ ...list, [e.target.name]: value });
   };
 
-  const upload = (items) => {
-    items.forEach((item) => {
-      const fileName = new Date().getTime() + item.label + item.file.name;
-      const storageRef = ref(storage, `items/${fileName}`);
-
-      const uploadTask = uploadBytesResumable(storageRef, item.file); // Upload the file.
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done.`);
-        },
-        (error) => {
-          // TODO: Handle unsuccessful uploads.
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-            setMovie((prev) => {
-              return { ...prev, [item.label]: url };
-            });
-            setUploaded((prev) => prev + 1);
-          });
-        }
-      );
-    });
-  };
-
-  const handleUpload = (e) => {
-    e.preventDefault();
-    upload([
-      { file: img, label: "img" },
-      { file: imgTitle, label: "imgTitle" },
-      { file: imgSm, label: "imgSm" },
-      { file: trailer, label: "trailer" },
-      { file: video, label: "video" },
-    ]);
+  const handleSelect = (e) => {
+    let value = Array.from(e.target.selectedOptions, (option) => option.value);
+    setList({ ...list, [e.target.name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createMovie(movie, dispatch);
+    createList(list, dispatch);
+    navigation("/lists");
   };
 
   return (
     <div className="newMovie">
-      <h1 className="newUser">New Movie</h1>
+      <h1 className="newUser">New List</h1>
       <form action="" className="newUserForm">
         <div className="newUserItem">
-          <label>Image</label>
-          <input
-            type="file"
-            id="img"
-            name="img"
-            onChange={(e) => setImg(e.target.files[0])}
-          />
-        </div>
-        <div className="newUserItem">
-          <label>Title Image</label>
-          <input
-            type="file"
-            placeholder="title"
-            id="imgTitle"
-            name="imgTitle"
-            onChange={(e) => setImgTitle(e.target.files[0])}
-          />
-        </div>
-        <div className="newUserItem">
-          <label>Thumbnail</label>
-          <input
-            type="file"
-            placeholder="title"
-            id="imgSm"
-            name="imgSm"
-            onChange={(e) => setImgSm(e.target.files[0])}
-          />
-        </div>
-        <div className="newUserItem">
           <label>Title</label>
-          <input type="text" placeholder="title" name="title" onChange={handleChange} />
-        </div>
-        <div className="newUserItem">
-          <label>Description</label>
           <input
             type="text"
-            placeholder="Description"
-            name="desc"
+            placeholder="title"
+            name="title"
             onChange={handleChange}
           />
         </div>
+
         <div className="newUserItem">
           <label>Genre</label>
           <input
             type="text"
             placeholder="Genre"
-            name="gender"
+            name="genre"
             onChange={handleChange}
           />
         </div>
+
         <div className="newUserItem">
-          <label>Year</label>
-          <input
-            type="text"
-            placeholder="Year"
-            name="year"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="newUserItem">
-          <label>Duration</label>
-          <input
-            type="text"
-            placeholder="Duration"
-            name="duration"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="newUserItem">
-          <label>Limit</label>
-          <input
-            type="number"
-            placeholder="Limit"
-            name="limit"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="newUserItem">
-          <label>Is Series?</label>
+          <label>Type</label>
           <select
-            name="isSerries"
-            id="isSerries"
+            name="type"
+            id="type"
             className="newUserSelect"
             onChange={handleChange}
           >
-            <option value="false">No</option>
-            <option value="true">Yes</option>
+            <option value="">Type</option>
+            <option value="series">Series</option>
+            <option value="movie">Movie</option>
           </select>
         </div>
+
         <div className="newUserItem">
-          <label>Trailer</label>
-          <input
-            type="file"
-            name="trailer"
-            onChange={(e) => setTrailer(e.target.files[0])}
-          />
+          <label>Content</label>
+          <select
+            type="text"
+            placeholder="content"
+            multiple
+            name="content"
+            onChange={handleSelect}
+          >
+            {movies.map((movie) => {
+              console.log(movie)
+              return <option key={movie._id} value={movie._id}>{movie.title}</option>
+            })}
+          </select>
         </div>
-        <div className="newUserItem">
-          <label>Video</label>
-          <input
-            type="file"
-            name="video"
-            onChange={(e) => setVideo(e.target.files[0])}
-          />
-        </div>
-        {uploaded === 5 ? (
-          <button className="newUserButton" onClick={handleSubmit}>
-            Create
-          </button>
-        ) : (
-          <button className="newUserButton" onClick={handleUpload}>
-            Upload
-          </button>
-        )}
+
+        <button className="newUserButton" onClick={handleSubmit}>
+          Create
+        </button>
       </form>
     </div>
   );
