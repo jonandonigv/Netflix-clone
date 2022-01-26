10675abeeb524f7cd40e3d97da3d42a72c5ade0e
jonandonigv <jonandonigv@gmail.com>
2022-01-26 16:53:54 +0100

    fetches the last users

--- admin/src/components/widgetSmall/WidgetSmall.jsx
+++ admin/src/components/widgetSmall/WidgetSmall.jsx
@@ -1,43 +1,57 @@
-import React, { useState } from 'react'
-import './widgetSmall.scss'
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import { useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./widgetSmall.scss";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function WidgetSmall() {
+  const [newUser, setNewUser] = useState([]);
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8000/api/users?new=true",
+          {
+            headers: {
+              token:
+                "Bearer " +
+                JSON.parse(localStorage.getItem("user")).accessToken, // Here goes the token
+            },
+          }
+        );
+        setNewUser(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getNewUsers();
+  }, []);
 
-    const [newUser, setNewUser] = useState([]);
-    useEffect(()=>{
-        const getNewUsers = async () => {
-            try {
-                const res =  await axios.get('http://localhost:8000/api/users?new=true', {headers: {
-                    token: 'Bearer ' + JSON.parse(localStorage.getItem("user")).accessToken // Here goes the token
-                }});
-                setNewUser(res.data)
-            } catch (error) {
-                console.log(error)
-            }
-        };
-        getNewUsers();
-    }, []);
-
-    return (
-        <div className='widgetSmall'>
-            <span className="widgetSmallTitle">New Join Members</span>
-            <ul className="widgetSmallList">
-                {newUser.map((user) => {
-                    <li className="widgetSmallListItem">
-                    <img src={user.profilePic || 'https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg'} alt="" className="widgetSmallImg" />
-                    <div className="widgetSmallUser">
-                        <span className="widgetSmallUsername">{user.username}</span>
-                    </div>
-                    <button className="widgetSmallButton">
-                        <VisibilityIcon className='widgetSmallIcon'/>
-                        Display
-                    </button>
-                </li>
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="widgetSmall">
+      <span className="widgetSmallTitle">New Join Members</span>
+      <ul className="widgetSmallList">
+        {newUser.map((user) => {
+          return (
+            <li className="widgetSmallListItem">
+              <img
+                src={
+                  "https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
+                }
+                alt=""
+                className="widgetSmallImg"
+              />
+              <div className="widgetSmallUser">
+                <span className="widgetSmallUsername">{user.username}</span>
+              </div>
+              <button className="widgetSmallButton">
+                <VisibilityIcon className="widgetSmallIcon" />
+                Display
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
