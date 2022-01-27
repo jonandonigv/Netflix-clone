09f886f422e5681e626d4d2abb3244bc515ae8d0
jonandonigv <jonandonigv@gmail.com>
2022-01-28 00:57:45 +0100

    stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,31 @@
-import './app.scss'
+import "./app.scss";
 import Home from "./pages/home/Home";
-import Login from './pages/Login/Login';
-import Register from './pages/register/Register';
-import Watch from './pages/watch/Watch';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom"
+import Login from "./pages/Login/Login";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/authContext/AuthContext";
+import { useContext } from "react";
 
 function App() {
-  const user = true;
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={user ? <Home /> : <Register/>}/>
-        <Route exact path='/register' element={!user ? <Register /> : <Home />}/>
-        <Route exact path='/login' element={!user ? <Login /> : <Home />}/>
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
+        <Route
+          exact
+          path="/register"
+          element={!user ? <Register /> : <Home />}
+        />
+        <Route exact path="/login" element={!user ? <Login /> : <Navigate to={'/'}/>} />
         {user && (
           <>
-            <Route path='/movies' element={<Home type='movies'/>}/>
-            <Route path='/series' element={<Home type='series'/>}/>
-            <Route path='/watch' element={<Watch />}/>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
           </>
         )}
-        
       </Routes>
     </Router>
   );
